ef52d54ad62387b468395d9dad5f1ea9e6df1fd3
Fuad Gulmammadov <fgulmemmedov1@std.qu.edu.az>
2018-01-18 13:15:38 +0400

    initial commit

---
+++ 20171108Win_BizimDialogBox/BizimDialoqBox.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _20171108Win_BizimDialogBox
+{
+    public partial class BizimDialoqBox : Form
+    {
+        public BizimDialoqBox()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.Yes;
+            this.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.No;
+            this.Close();
+        }
+    }
+}
---
+++ 20171108Win_BizimDialogBox/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _20171108Win_BizimDialogBox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //MessageBox.Show("Test");
+            BizimDialoqBox bizimDialoq = new BizimDialoqBox();
+            DialogResult netice = bizimDialoq.ShowDialog();
+
+            if (netice == DialogResult.Yes)
+            {
+                label1.Text = "Gonder Buttonu klik oldu";
+                label1.Text += "\n" + bizimDialoq.textBox1.Text;
+            }
+            else if (netice == DialogResult.No)
+            {
+                label1.Text = "Legv et buttonu klik oldu";
+            }
+            else
+            {
+                label1.Text = "Basqa button klik oldu";
+            }
+
+           
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                label1.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (fontDialog1.ShowDialog() == DialogResult.OK)
+            {
+                label1.Font = fontDialog1.Font;
+            }
+        }
+    }
+}
